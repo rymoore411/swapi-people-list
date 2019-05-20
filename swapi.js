205b7e63eec442630ca0491a33bd4ec82c1f44eb
Ryan Moore <rymoore411@gmail.com>
2019-05-19 18:00:16 -0700

    need put info to right of list

--- swapi.js
+++ swapi.js
@@ -1,17 +1,57 @@
 
 
 async function starWars (){
-  const response = await fetch('https://swapi.co/api/people/');
-  const data = await response.json();
-  const peopleObj = data.results;
+  let response = await fetch('https://swapi.co/api/people/');
+  let data = await response.json();
+  let allPeople = [];
+  let peopleObj = [];
 
-  const person = peopleObj.map(p => p.name);
+  //loop through all pages to get all characters
+  while(data.next){
+    peopleObj.push(data.results);
+    let person = data.results.map(p => p.name);
+    allPeople.push(person);
+    response = await fetch(data.next);
+    data = await response.json();
 
-  const html = person.map(nm => `<li>${nm}</li>`).join('');
+  }
 
-  const selector = document.querySelector('ul');
+    console.log(peopleObj[0]);
+    //get all characters in single array
+    let people = [];
+    allPeople.forEach(el => {
+      el.forEach(nm => {
+        people.push(nm);
+      });
+    });
 
-  selector.innerHTML = html;
+    //displays all characters in list
+    let html = people.map(nm => `<li>${nm}</li>`).join('');
+    const selector = document.querySelector('ul');
+    selector.innerHTML = html;
+
+    //add listeners
+    let personInfo;
+
+    selector.addEventListener('click', (ev) => {
+      let personSelect = ev.target.innerHTML;
+
+      peopleObj.forEach((el, idx) => {
+        el.forEach(personInfo => {
+          if(personInfo.name === personSelect){
+            console.log(personInfo);
+            let infoList = Object.keys(personInfo).map(key => `
+            <div><b>${key }</b> ${personInfo[key]}</div>`).join("");
+            let div = document.getElementById('info');
+            div.innerHTML = infoList;
+          }
+        });
+
+      });
+     });
+
 }
+
+
 
 starWars();
